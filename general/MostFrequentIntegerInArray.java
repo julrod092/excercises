74468232fa269be2c8a2fe28ca33977fcd8bece8
Julian Rodriguez <julianrodriguez@MacBook-Pro-de-Julian.local>
2015-10-13 22:16:09 -0500

    General Exercise 1: Find the most frequent integer in an array

---
+++ general/MostFrequentIntegerInArray.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MostFrequentIntegerInArray {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Write the length of the array: ");
+        Integer n = in.nextInt();
+        Integer []array = new Integer[n];
+
+        for (Integer i = 0 ; i < n ; i++) {
+            array[i] = in.nextInt();
+        }
+
+        Integer result = mostFrequentInteger(array);
+        System.out.print(result);
+    }
+
+    public static Integer mostFrequentInteger (Integer []array){
+
+        Arrays.sort(array);
+
+        Integer result = 0;
+        Integer present = array[0];
+        Integer frequent = array[0];
+        Integer count = 1;
+        Integer pastCount = 1;
+
+        for (Integer i = 1 ; i < array.length ; i++) {
+          if (present == array[i]) {
+            count += 1;
+          } else {
+            frequent = (count > pastCount) ? frequent = present : frequent;
+            present = array[i];
+            pastCount = (count > pastCount) ? count : pastCount;
+            count = 1;
+          }
+        }
+
+        return result = (pastCount == count) ? 0 : ((pastCount > count) ? frequent : present);
+    }
+}
